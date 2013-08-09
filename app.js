dbd18510a6c2fe9448d7884e7355f4de4ffd65b5
Scot Matson <sjpmatson@gmail.com>
2013-08-08 17:39:46 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+var projectManager = angular.module('projectManager', [])
+
+.config(function ($routeProvider) {
+  $routeProvider
+		.when('/',
+			{
+				templateUrl: './app/partials/listProjects.html',
+				controller:  'projectData'
+			})
+		.when('/projectDetails/:title',
+			{
+				templateUrl: './app/partials/projectDetails.html',
+				controller:  'projectFinder'
+			})
+		.when('/addProject',
+			{
+				templateUrl: './app/partials/addProject.html',
+				controller:  'addController'
+			})
+		.when('/deleteProject',
+			{
+				templateUrl: './app/partials/deleteProject.html',
+				controller:  'deleteController'
+			})
+		.when('/editProject',
+			{
+				templateUrl: './app/partials/editProject.html',
+				controller:  'editController'
+			})
+		.otherwise({ redirectTo: '/' });
+});
