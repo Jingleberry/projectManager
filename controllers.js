b382a8cb475cd080fce8551330ca8a5085665689
Scot Matson <sjpmatson@gmail.com>
2013-08-09 18:07:38 -0700

    Update controllers.js

--- controllers.js
+++ controllers.js
@@ -1,15 +1,20 @@
+// TODO Cancellation function
+// TODO Status by selection - Not Started, On Track, Delayed, Completed
+// TODO Commenting BLOG, Push new posts to top.
+// TODO Date picker for due dates
+// TODO Team member indicator tags
+// TODO Hyperlink support in comments
+
 // Creates a controller object
 var controllers = {};
 
-// Grabs all project data
+// Retrives all project data
 controllers.projectData = function ($scope, dataFactory) {
 	$scope.projects = dataFactory.getAllProjects();
 };
 
 // Locates project details, creates functionality to manipulate stored data
-controllers.projectFinder = function ($scope, $routeParams, dataFactory) {
-	
-	// Queries the projects DB and stores in a $scope variable
+controllers.projectController = function ($scope, $routeParams, dataFactory) {
 	$scope.projects = dataFactory.getAllProjects();
 	
 	// Finds an index within the project Array
@@ -22,10 +27,11 @@ controllers.projectFinder = function ($scope, $routeParams, dataFactory) {
 		}
 	};
 	$scope.project = findProject();
-	return findProject();
+
 };
 
-// This works. New projects are added. Errors if fields are undefined
+// Adds new projects
+// TODO, error handling / validation
 controllers.addController = function ($scope, dataFactory) {
 		$scope.projects = dataFactory.getAllProjects();
 		$scope.addProject = function () {
@@ -37,36 +43,6 @@ controllers.addController = function ($scope, dataFactory) {
 				launch: $scope.newLaunch.launch
 			});
 		};
-};
-
-
-// So far... this takes the appropriate Array Index and
-// populates an input form with existing values.
-// I need to find a way to edit the data and commit it back
-// to the Array Index.
-controllers.editController = function ($scope, $routeParams, dataFactory) {
-	
-	// Queries the projects DB and stores in a $scope variable
-	$scope.projects = dataFactory.getAllProjects();
-	
-	// Finds an index within the project Array
-	var findProject = function () {
-		for(var i = 0; i < $scope.projects.length; ++i) {
-			var project = $scope.projects[i];
-			if(project.title === $routeParams.title) {
-				return project;
-			}
-		}
-	};
-
-	// These are the values I want to change, but I don't know how to do that.
-	console.log($scope.project.title);
-	console.log($scope.project.author);
-	console.log($scope.project.status);
-	console.log($scope.project.launch);
-	console.log(findProject());
-
-	$scope.project = findProject();
 };
 
 // Passes all controllers to the projectManager App
